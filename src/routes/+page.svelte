3a0f15eb017239caab717fa0dce183864a4b96b0
Eric Hamilton <ericjean873@gmail.com>
2025-10-15 16:38:11 -0700

    handle fetching of handles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,27 @@
 <script lang="ts">
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
 	import { PDS_URL } from '$lib/constants';
-	import type { ComAtprotoSyncListRepos } from "@atcute/atproto";
+	import type { ComAtprotoRepoDescribeRepo, ComAtprotoSyncListRepos } from '@atcute/atproto';
 
-	let repos = $state<ComAtprotoSyncListRepos.Repo[]>([]);
+	let repos = $state<ComAtprotoRepoDescribeRepo.$output[]>([]);
 
 	onMount(async () => {
-	  let response = await fetch(`${PDS_URL}/xrpc/com.atproto.sync.listRepos`);
-	  let data: ComAtprotoSyncListRepos.$output = await response.json();
-		repos = data.repos;
-		console.log(repos);
+		let response = await fetch(`${PDS_URL}/xrpc/com.atproto.sync.listRepos`);
+		let data: ComAtprotoSyncListRepos.$output = await response.json();
+		for (let repo of data.repos) {
+			await fetch(`${PDS_URL}/xrpc/com.atproto.repo.describeRepo?repo=${repo.did}`)
+				.then((response) => response.json())
+				.then((data) => repos.push(data));
+		}
 	});
 </script>
 
 <div>
-  {#if repos.length > 0}
-    {#each repos as repo (repo.did)}
-      {#if repo.active}
-        <div>{repo.did}</div>
-      {/if}
-    {/each}
-  {:else}
-    <div>No repositories found.</div>
-  {/if}
+	{#if repos.length >= 3}
+		{#each repos as repo (repo.did)}
+			<div>{repo.handle}: {repo.did}</div>
+		{/each}
+	{:else}
+		<div>Loading Repos...</div>
+	{/if}
 </div>
