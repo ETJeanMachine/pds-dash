2185b396a8d3ebe7f2c47e33f62b5be5b5096800
Eric Hamilton <ericjean873@gmail.com>
2025-10-15 13:05:45 -0700

    inital commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div class="layout">
+  {@render children?.()}
+</div>
+
+<style>
+  :global(html),
+  :global(body) {
+    background-color: black;
+    color: white;
+  }
+
+  .layout {
+    display: flex;
+    flex-direction: column;
+  }
+</style>
